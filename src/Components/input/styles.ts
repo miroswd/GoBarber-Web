a84dbfde5fa3cc3deb4216d3aee79c7305beec95
miroswd <altamirsantos93@hotmail.com>
2020-05-30 16:54:16 -0300

    Validando dados

--- src/Components/input/styles.ts
+++ src/Components/input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -22,6 +25,13 @@ export const Container = styled.div<ContainerProps>`
     margin-top: 8px;
   }
 
+  /* Mantendo o error em cima, para quando clicar, o focused assuma o poder*/
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
   ${props =>
     props.isFocused &&
     css`
@@ -34,6 +44,9 @@ export const Container = styled.div<ContainerProps>`
     css`
       color: #ff9000;
     `}
+
+
+
   input {
     flex: 1;
     background: transparent;
@@ -49,5 +62,22 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+// Aplicando estilos no container do tooltip
+// Como está passando de um elemento superior, tem q permitir no className
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+  span {
+    background: #c53030;
+    color: #fff;
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
